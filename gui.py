5bd28d6f6e45dd0eef27b1336b9a26b47a6212ca
gabrielleari <gabrielle.ari222@gmail.com>
2026-05-10 17:38:41 -0500

    added some functionality to turntable class

--- gui.py
+++ gui.py
@@ -1,13 +1,38 @@
+import maya.cmds as cmds
 import PySide6.QtWidgets as qw
 import sys
 
 sys.path.insert(0, 'C:\Program Files\Autodesk\Maya2026\scripts')
 
 
+class TurnaroundTool:
+
+    def __init__(self):
+        self.turntable = None
+
+    def create_turntable(self):
+        if cmds.objExists("turntable_grp"):
+            cmds.delete("turntable_grp")
+
+        self.turntable = cmds.group(empty=True, name="turntable_grp")
+
+        rot_grp = cmds.group(
+            empty=True,
+            name="turntable_rotate",
+            parent=self.turntable
+        )
+
+        cmds.addAttr(self.turntable, ln="rotateGroup", dt="string")
+        cmds.setAttr( f"{self.turntable}.rotateGroup", rot_grp, type="string")
+
+
 class GUIUI(qw.QDialog):
 
     def __init__(self):
         super(GUIUI, self).__init__()
+
+        self.tool = TurnaroundTool()
+
         self.setWindowTitle("Turntable and Playblast Tool")
         self.setFixedWidth(500)
 
@@ -34,10 +59,12 @@ class GUIUI(qw.QDialog):
         self.frames.setValue(120)
 
         create_tt = qw.QPushButton("Create Turn Table")
+        create_tt.clicked.connect(self.create_turntable)
+
         create_pb = qw.QPushButton("Create Playblast")
         remove_tt = qw.QPushButton("Remove Turn Table")
-        close = qw.QPushButton("Close")
 
+        close = qw.QPushButton("Close")
         close.clicked.connect(self.close)
 
         layout = qw.QFormLayout(self)
@@ -48,6 +75,9 @@ class GUIUI(qw.QDialog):
         layout.addRow(create_pb)
         layout.addRow(remove_tt)
         layout.addRow(close)
+
+    def create_turntable(self):
+        self.tool.create_turntable()
 
 
 def launch_ui():
